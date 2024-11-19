c294a4cdb19ec26b22b3771e27230564285b9d3f
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-19 14:27:21 +0100

    Add project files.

---
+++ IfElse Oppgave 2 emne 3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number1 = 5;
+int number2 = 5;
+bool isEqual = false;
+
+int Run()
+{
+    if (number1 == number2)
+    {
+        isEqual = true;
+        Console.WriteLine("tallene er like");
+    }
+    else
+    {
+        isEqual = false;
+        Console.WriteLine("tallene er ulike");
+    }
+
+    if (isEqual)
+    {
+        Console.WriteLine($"summen er:{number1*number2}");
+    }
+    else
+    {
+       Console.WriteLine($"summen er:{number1+number2}");
+    }
+
+    return number1;
+}
+
+Run();
